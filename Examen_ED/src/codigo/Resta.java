bf7c4d139774054d898f466aa1258c5ce2262684
Alex <Alex@Alex-PC>
2014-06-11 21:29:04 +0200

    KHO

--- Examen_ED/src/codigo/Resta.java
+++ Examen_ED/src/codigo/Resta.java
@@ -8,20 +8,26 @@ public class Resta {
 	//Inicialización
 	private int a;
 	private int b;
+	
+
+public static void main (String[] args){
+	
+}
 
 	//Constructor
-	public Resta() {
-		a=0;
-		b=0;
+	public Resta(int a,int b) {
+		this.a=a;
+		this.b=b;
 	}
 	
 	public void setA(int a){
 		this.a=a;
 	}
-
+	
 	public void setB(int b){
-		this.a=a;
+		this.b=b;
 	}
+	
 	
 	//Método que permite restar dos números enteros
 	public int resta(){
